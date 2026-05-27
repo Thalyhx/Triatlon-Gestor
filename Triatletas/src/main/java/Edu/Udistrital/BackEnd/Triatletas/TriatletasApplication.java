ca76701631a06f61f321fefbd96d2a9ed643a4fe
Nath <nathalycis20@gmail.com>
2026-05-26 23:11:16 -0500

    Creando microservicio triatleta

---
+++ Triatletas/src/main/java/Edu/Udistrital/BackEnd/Triatletas/TriatletasApplication.java
@@ -0,0 +1,13 @@
+package Edu.Udistrital.BackEnd.Triatletas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriatletasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriatletasApplication.class, args);
+	}
+
+}
---
+++ Triatletas/src/test/java/Edu/Udistrital/BackEnd/Triatletas/TriatletasApplicationTests.java
@@ -0,0 +1,13 @@
+package Edu.Udistrital.BackEnd.Triatletas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriatletasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
