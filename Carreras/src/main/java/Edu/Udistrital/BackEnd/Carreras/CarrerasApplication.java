725c76a8aa0f35a311be58b2ba39df65dd9f53a0
Nath <nathalycis20@gmail.com>
2026-05-26 23:22:20 -0500

    Creando microservicio carrera

---
+++ Carreras/src/main/java/Edu/Udistrital/BackEnd/Carreras/CarrerasApplication.java
@@ -0,0 +1,13 @@
+package Edu.Udistrital.BackEnd.Carreras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrerasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrerasApplication.class, args);
+	}
+
+}
---
+++ Carreras/src/test/java/Edu/Udistrital/BackEnd/Carreras/CarrerasApplicationTests.java
@@ -0,0 +1,13 @@
+package Edu.Udistrital.BackEnd.Carreras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrerasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
