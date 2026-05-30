3a9fdc61924183e92c8a8a3d53c22dcbc1c82dee
Nath <nathalycis20@gmail.com>
2026-05-30 13:35:10 -0500

    Añadiendo configuraciones a categtoria

---
+++ Categorias/src/main/java/Edu/Udistrital/BackEnd/Categorias/Config/CategoriaConfig.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Edu.Udistrital.BackEnd.Categorias.Config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author nath
+ */
+@Configuration
+public class CategoriaConfig {
+    
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder()
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+    
+    @Bean 
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") 
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+    
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+    
+}
